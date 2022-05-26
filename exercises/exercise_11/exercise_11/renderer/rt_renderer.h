266c5fb0bc41d4c83b1d562f612706ef4f244c11
Adam Saidane <adamsaidane252@gmail.com>
2022-05-26 14:15:44 +0200

    stuff

--- exercises/exercise_11/exercise_11/renderer/rt_renderer.h
+++ exercises/exercise_11/exercise_11/renderer/rt_renderer.h
@@ -11,39 +11,42 @@
 #include "rt_types.h"
 #include "frame_buffer.h"
 
-namespace rt{
+namespace rt
+{
     using namespace Colors;
     using namespace glm;
 
-    class Renderer{
+    class Renderer
+    {
 
         const unsigned int max_recursion = 5;
         float p_rg = 0.4f;
 
     public:
-        void render(const std::vector<vertex> &vts,
-                    const glm::mat4 &m,
-                    const glm::mat4 &v,
-                    const float fov_degrees,
-                    unsigned int depth,
-                    FrameBuffer <uint32_t> &fb) {
+        void render( const std::vector<vertex> &vts,
+                     const glm::mat4 &m,
+                     const glm::mat4 &v,
+                     const float fov_degrees,
+                     unsigned int depth,
+                     FrameBuffer<uint32_t> &fb )
+        {
 
             float aspect_ratio = fb.H / fb.W;
             // we use the fov and the tangent function to compute where is the bottom of the projection plane,
             // we assume that the projection place is 1 unit in front of the camera (z == -1)
-            float bottom = - tan(abs(radians(fov_degrees)) * 0.5f);
+            float bottom = -tan( abs( radians( fov_degrees )) * 0.5f );
 
             // find the transformation that move points from camera space to model space
-            mat4 view_to_model = inverse(v * m);
+            mat4 view_to_model = inverse( v * m );
             // the bottom left corner of the image plane/camera sensor
-            vec4 lower_left_corner = vec4(bottom * aspect_ratio, bottom, -1, 1);
+            vec4 lower_left_corner = vec4( bottom * aspect_ratio, bottom, -1, 1 );
             // we transform the camera position (also the convergence point of light rays) from camera coordinates to model coordinates
             // notice that we implicitly assume that the camera position is at 0,0,0 in its one coordinate space
-            vec4 cam_pos = view_to_model * vec4(0,0,0,1);
+            vec4 cam_pos = view_to_model * vec4( 0, 0, 0, 1 );
 
             // the distance from the center of one pixel to the next along the horizontal and vertical axes of the screen
             // notice that * and / are applied component wise
-            vec2 pixel_size = abs(vec2(lower_left_corner)) * 2.0f / vec2(fb.H, fb.W);
+            vec2 pixel_size = abs( vec2( lower_left_corner )) * 2.0f / vec2( fb.H, fb.W );
 
 
             // TODO ex 11.1 iterate through all pixels in the buffer (width: [0, fb.W), height:[0, fb.H])
@@ -55,20 +58,27 @@ namespace rt{
             //  all intersection computations should happen in the same space, no matter what that space is)
             //  - create a ray with the camera origin, and the vector from the camera origin to the pixel you have just found
             //  - call the TraceRay method using that ray, and store the resulting color in the frame buffer (fb)
-            fb.paintAt(fb.W/2, fb.H/2, toRGBA32(white));
-
+            fb.paintAt( fb.W / 2, fb.H / 2, toRGBA32( white ));
+            for ( int x = 0; x < fb.W; x++ )
+            {
+                for ( int y = 0; y < fb.H; y++ )
+                {
+                    auto pixel = fb.valueAt( x, y );
+                }
+            }
         }
 
 
-        color TraceRay(const Ray & ray,
-                       unsigned int depth,
-                       const std::vector<vertex> &vts){
+        color TraceRay( const Ray &ray,
+                        unsigned int depth,
+                        const std::vector<vertex> &vts )
+        {
             // this is here to ensure we don't end up with a long recurssion that can freeze the program (or a stack overflow)
             depth = depth > max_recursion ? max_recursion : depth;
 
             color col = black; // used to output a color
             Hit hitInfo; // used to store the hit information
-            if (! RayModelIntersection(ray, vts, hitInfo)) return col; // no hit, return black
+            if ( !RayModelIntersection( ray, vts, hitInfo )) return col; // no hit, return black
 
 
             // TODO ex 11.2 replace the current i_normal and i_col computation with their interpolated versions
@@ -79,16 +89,18 @@ namespace rt{
 
 
             // TODO ex 11.3 implement the phong reflection model for the point light below
-            vec3 light_pos(0,1.9f,0); // light position in model space
+            vec3 light_pos( 0, 1.9f, 0 ); // light position in model space
             col = i_col; // set the light reflection color here
 
 
             // the recursion/reflection happens here!
-            if (depth > 1) {
-                Ray reflected_ray(i_pos, reflect(ray.direction, i_normal));
-                reflected_ray.origin -= ray.direction * .001f; // this is a small offset to address numerical precision issues
+            if ( depth > 1 )
+            {
+                Ray reflected_ray( i_pos, reflect( ray.direction, i_normal ));
+                reflected_ray.origin -=
+                        ray.direction * .001f; // this is a small offset to address numerical precision issues
                 // integrate the current color with the reflection color by a p_rg factor
-                col += p_rg * TraceRay(reflected_ray, depth - 1, vts);
+                col += p_rg * TraceRay( reflected_ray, depth - 1, vts );
             }
 
             return col;
@@ -96,16 +108,18 @@ namespace rt{
 
         // returns false if no intersection
         // intersection results are returned in the "hit" reference variable
-        static bool RayModelIntersection(const Ray & ray,
-                                         const std::vector<vertex> &vts,
-                                         Hit &hit){
-            for (int i = 0; i < vts.size(); i+=3)
+        static bool RayModelIntersection( const Ray &ray,
+                                          const std::vector<vertex> &vts,
+                                          Hit &hit )
+        {
+            for ( int i = 0; i < vts.size(); i += 3 )
             {
                 float dist_temp = FLT_MAX;
                 vec3 barycentric_temp;
                 // notice that we use the hit.dist to ensure that if new intersections happen, they are closer to the
                 // convergence point than the previously stored hit.
-                if (RayTriangleIntersection(ray, vts[i], vts[i+1], vts[i+2], dist_temp, barycentric_temp) && dist_temp < hit.dist)
+                if ( RayTriangleIntersection( ray, vts[i], vts[i + 1], vts[i + 2], dist_temp, barycentric_temp ) &&
+                     dist_temp < hit.dist )
                 {
                     hit.hit_ID = i;
                     hit.dist = dist_temp;
@@ -116,40 +130,40 @@ namespace rt{
         }
 
         // returns false if no intersection
-        static bool RayTriangleIntersection(const Ray & ray,
-                                            const vertex & p1,
-                                            const vertex & p2,
-                                            const vertex & p3,
-                                            float & t, vec3 & barycentric)
+        static bool RayTriangleIntersection( const Ray &ray,
+                                             const vertex &p1,
+                                             const vertex &p2,
+                                             const vertex &p3,
+                                             float &t, vec3 &barycentric )
         {
             vec3 e1 = p2.pos - p1.pos;
             vec3 e2 = p3.pos - p1.pos;
-            vec3 q = cross(ray.direction, e2);
-            float a = dot(e1, q);
+            vec3 q = cross( ray.direction, e2 );
+            float a = dot( e1, q );
 
             float tolerance = 10e-7f;
             // for numerical stability, a = 0 means that triangle plane and ray are parallel
-            if (abs(a) < tolerance) return false;
+            if ( abs( a ) < tolerance ) return false;
 
             float f = 1.0f / a;
-            vec3 s = ray.origin - vec3(p1.pos);
-            float u = f * dot(s, q);
+            vec3 s = ray.origin - vec3( p1.pos );
+            float u = f * dot( s, q );
 
             // if u < 0, intersection with plane is not within the triangle
-            if (u < -tolerance) return false;
+            if ( u < -tolerance ) return false;
 
-            vec3 r = cross(s, e1);
-            float v = f * dot(ray.direction, r);
+            vec3 r = cross( s, e1 );
+            float v = f * dot( ray.direction, r );
 
             // if v < 0 or u+v > 1, intersection with plane is not within the triangle
-            if (v < -tolerance || u + v > 1) return false;
+            if ( v < -tolerance || u + v > 1 ) return false;
 
-            t = f * dot(e2, r);
+            t = f * dot( e2, r );
 
-            if (t < 0)
+            if ( t < 0 )
                 return false;
 
-            barycentric = vec3(1.0f - u - v, u, v);
+            barycentric = vec3( 1.0f - u - v, u, v );
 
             return true;
         }
--- exercises/exercise_9/shaders/compose.frag
+++ exercises/exercise_9/shaders/compose.frag
@@ -6,10 +6,12 @@ uniform sampler2D SourceTexture;
 //TODO 9.4 : Add bloom texture sampler
 
 
-
 //TODO 9.1 and 9.2 : Add uniforms here
-
-
+uniform float exposure;
+uniform float contrast;
+uniform float hue;
+uniform float saturation;
+uniform vec3 colorFilter;
 
 
 // variables from vertex shader
@@ -22,60 +24,55 @@ out vec4 FragColor;
 
 float GetLuminance(vec3 color)
 {
-   return dot(color, vec3(0.2126f, 0.7152f, 0.0722f));
+    return dot(color, vec3(0.2126f, 0.7152f, 0.0722f));
 }
 
 vec3 RGBToHSV(vec3 rgb)
 {
-   vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
 
-   vec4 p = mix( vec4( rgb.bg, K.wz ), vec4( rgb.gb, K.xy ), step( rgb.b, rgb.g ) );
-   vec4 q = mix( vec4( p.xyw, rgb.r ), vec4( rgb.r, p.yzx ), step( p.x, rgb.r ) );
+    vec4 p = mix(vec4(rgb.bg, K.wz), vec4(rgb.gb, K.xy), step(rgb.b, rgb.g));
+    vec4 q = mix(vec4(p.xyw, rgb.r), vec4(rgb.r, p.yzx), step(p.x, rgb.r));
 
-   float d = q.x - min( q.w, q.y );
+    float d = q.x - min(q.w, q.y);
 
-   float epsilon = 1.0e-10;
+    float epsilon = 1.0e-10;
 
-   return vec3( abs(q.z + (q.w - q.y) / (6.0 * d + epsilon)), d / (q.x + epsilon), q.x);
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + epsilon)), d / (q.x + epsilon), q.x);
 }
 
-vec3 HSVToRGB( vec3 hsv )
+vec3 HSVToRGB(vec3 hsv)
 {
-   vec4 K = vec4( 1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0 );
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
 
-   vec3 p = abs( fract( hsv.xxx + K.xyz ) * 6.0 - K.www );
+    vec3 p = abs(fract(hsv.xxx + K.xyz) * 6.0 - K.www);
 
-   return hsv.z * mix( K.xxx, clamp(p - K.xxx, 0, 1), hsv.y );
+    return hsv.z * mix(K.xxx, clamp(p - K.xxx, 0, 1), hsv.y);
 }
 
 
 void main()
 {
-   vec3 hdrColor = texture(SourceTexture, textureCoordinates).rgb;
+    vec3 hdrColor = texture(SourceTexture, textureCoordinates).rgb;
 
-   //TODO 9.4 : Sample bloom texture and add the bloom to HDR color
-
-
-
-   //TODO 9.1 : Apply tone mapping using the exposure uniform
-   vec3 color = hdrColor;
+    //TODO 9.4 : Sample bloom texture and add the bloom to HDR color
 
 
-   //TODO 9.2 : Modify contrast
+    //TODO 9.1 : Apply tone mapping using the exposure uniform
+    vec3 color = hdrColor;
+    color += vec3(1.0) - exp(-hdrColor * exposure);
+
+    //TODO 9.2 : Modify contrast
+    vec3 contastedColor = clamp((color - vec3(0.5, 0.5, 0.5))* contrast + vec3(0.5, 0.5, 0.5), 0, 1);// subtract gray
+    color += contastedColor;
+    //    color *= contrast;
+    //    color += vec3(0.5, 0.5, 0.5);
+    //TODO 9.2 : Modify hue
+    //TODO 9.2 : Modify saturation
+    // color *= HSVToRGB(vec3(hue, saturation, contrast));
+
+    //TODO 9.2 : Apply color filter
 
 
-
-   //TODO 9.2 : Modify hue
-
-
-
-   //TODO 9.2 : Modify saturation
-
-
-
-   //TODO 9.2 : Apply color filter
-
-
-
-   FragColor = vec4(color, 1.0f);
+    FragColor = vec4(color, 1.0f);
 }
